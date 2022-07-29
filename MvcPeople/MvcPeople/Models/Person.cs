184cbf189ebd9ee3fd8c9e907cb986bb6bd6f9bb
Mario Beltran <mariobeltran1999@gmail.com>
2022-07-29 01:13:26 -0600

    :sparkles: add mvc people

---
+++ MvcPeople/MvcPeople/Models/Person.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+
+namespace MvcPeople.Models
+{
+    public class Person
+    {
+        public int dui { get; set; }
+        public string name { get; set; }
+        public string surname { get; set; }
+        public DateTime dateBirthday { get; set; }
+        public string address { get; set; }
+        public string email { get; set; }
+    }
+}
